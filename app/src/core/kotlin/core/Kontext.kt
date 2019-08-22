ac166fd359649b9d3d7620bb350c6efb4fd1b31f
kar <hi@kar.gs>
2019-08-22 16:06:50 +0200

    wip

--- app/src/core/kotlin/core/Kontext.kt
+++ app/src/core/kotlin/core/Kontext.kt
@@ -1,29 +1,11 @@
 package core
 
-import kotlinx.coroutines.Dispatchers.Unconfined
-import kotlin.coroutines.CoroutineContext
-
 open class Kontext internal constructor(
-        private val id: Any,
-        private val emit: Emit = DefaultEmit(id.toString()),
-        val coroutineContext: () -> CoroutineContext = { throw Exception("coroutineContext not linked") }
-): Emit by emit {
+        private val id: Any
+) {
 
     companion object {
         fun new(id: Any) = Kontext(id)
-
-        fun forCoroutine(coroutineContext: CoroutineContext, id: String) = Kontext(
-                id = id,
-                coroutineContext = { coroutineContext }
-        )
-
-        fun forTest(id: String = "test",
-                    coroutineContext: CoroutineContext = Unconfined) = Kontext(
-                id = id,
-                coroutineContext = { coroutineContext },
-                emit = CommonEmit(ktx = { Kontext("$id:emit",
-                        coroutineContext = { coroutineContext }) })
-        )
     }
 }
 
